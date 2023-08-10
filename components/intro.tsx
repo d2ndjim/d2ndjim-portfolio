6df88691f108a6daf225399c19d3f64e77eeb315
Lekan <jimoridwanlekan@gmail.com>
2023-08-10 15:21:25 +0100

    Add animation to header scroll

--- components/intro.tsx
+++ components/intro.tsx
@@ -8,16 +8,16 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import picture from "@/public/orange-edit.png";
 
-// import { useSectionInView } from "@/lib/hooks";
+import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Intro() {
-  // const { ref } = useSectionInView("Home", 0.5);
+  const { ref } = useSectionInView("Home", 0.5);
   const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
   return (
     <section
-      // ref={ref}
+      ref={ref}
       id="home"
       className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
     >
