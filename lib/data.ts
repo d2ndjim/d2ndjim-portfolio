a875bfb5595e7ac82f07aa44e37503ad2909de8c
Lekan <jimoridwanlekan@gmail.com>
2023-08-11 07:58:39 +0100

    Add experience section

--- lib/data.ts
+++ lib/data.ts
@@ -1,7 +1,6 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
 import kwisellImg from "@/public/kwiksell.png";
 import storeImg from "@/public/ecomm-store.png";
 import genieImg from "@/public/genie.png";
@@ -38,24 +37,24 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Software Engineer",
+    location: "PAYSLICE",
     description:
       "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    icon: React.createElement(CgWorkAlt),
+    date: "2022 - 2023",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "Software Developer",
+    location: "SOFTCOM LIMITED",
     description:
       "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
     icon: React.createElement(CgWorkAlt),
     date: "2019 - 2021",
   },
   {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
+    title: "Technical Mentor (Volunteer)",
+    location: "MICROVERSE",
     description:
       "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
     icon: React.createElement(FaReact),
